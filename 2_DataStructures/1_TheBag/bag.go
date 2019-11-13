0e87805bc1883d87fe8e98941dffde5417f1086e
Austin Orth <aorth@niche.com>
2019-11-12 19:15:59 -0500

    Homework 1 complete
    Completed the first homework assignment for class!

--- 2_DataStructures/1_TheBag/bag.go
+++ 2_DataStructures/1_TheBag/bag.go
@@ -4,6 +4,7 @@ package bag
 // for adding and removing items, checking if a given item exists in the bag,
 // and getting the number of items stored in the bag.
 type Bag struct {
+	Contents []int
 	// TODO: Add fields here for storing items. You can use any data structure
 	// you want (there is no "right" answer). The fields you add here will be
 	// accessible in the methods below. See: https://gobyexample.com/methods
@@ -11,16 +12,13 @@ type Bag struct {
 
 // NewBag is used to create an instance of a Bag.
 func NewBag() *Bag {
-	return &Bag{
-		// TODO: If necessary, initialize fields here.
-	}
+	return &Bag{nil}
 }
 
 // Add inserts an item into the bag. The bag should accept and store duplicate
 // items (i.e. it is not a set).
 func (b *Bag) Add(i int) {
-	// TODO: Implement the Add method.
-
+	b.Contents = append(b.Contents, i)
 	return
 }
 
@@ -29,21 +27,32 @@ func (b *Bag) Add(i int) {
 // exist in the bag, it should silently do nothing.
 func (b *Bag) Remove(i int) {
 	// TODO: Implement the Remove method.
-
+	c := b.Contents
+	for index, item := range c {
+		if item == i {
+			c[index] = c[len(c)-1]
+			c[len(c)-1] = 0
+			c = c[:len(c)-1]
+			b.Contents = c
+			return
+		}
+	}
 	return
 }
 
 // Contains returns true if the given item exists in the bag, and false
 // otherwise.
 func (b *Bag) Contains(i int) bool {
-	// TODO: Implement the Contains method.
-
+	c := b.Contents
+	for _, item := range c {
+		if item == i {
+			return true
+		}
+	}
 	return false
 }
 
 // Size returns the number of items in the bag.
 func (b *Bag) Size() int {
-	// TODO: Implement the size method.
-
-	return 0
+	return len(b.Contents)
 }
